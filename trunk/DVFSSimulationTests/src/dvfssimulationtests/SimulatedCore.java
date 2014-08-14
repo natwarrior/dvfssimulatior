c870f8974b5149fd7136350f9610fa7f55c3d08c
ca.magno00@gmail.com <ca.magno00@gmail.com@c93d529b-89e6-e009-402c-ae1dc9cd3e2b>
2014-08-14 17:19:57 +0000

    Initial architecture.

--- trunk/DVFSSimulationTests/src/dvfssimulationtests/SimulatedCore.java
+++ trunk/DVFSSimulationTests/src/dvfssimulationtests/SimulatedCore.java
@@ -6,10 +6,40 @@
 
 package dvfssimulationtests;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author carlosmagno
  */
-public class SimulatedCore {
+public class SimulatedCore extends Thread{
     
+    private int cIdx;
+    private int jobSize;
+    private double consumedEnergy;
+    
+    public SimulatedCore(int n, int js){
+        cIdx = n;
+        jobSize = js;
+    }
+    
+    public void executeJob() throws EndOfJobException{
+        run();
+        throw new EndOfJobException(cIdx);
+    }
+    
+    @Override
+    public void run(){
+        System.out.println("Core " + cIdx + " iniciando tarefa...");
+        for(int doneJob = 0; doneJob <= jobSize ; doneJob++){
+            try {
+                Thread.sleep(SimulatedCPU.MAX_FREQ - SimulatedCPU.freqs[cIdx]);
+            } catch (InterruptedException ex) {
+                System.exit(1);
+            }
+            int percent =  (doneJob*100)/jobSize;
+            System.out.println("Core " + cIdx + " concluiu " + percent +"% da tarefa.");
+        }
+    }
 }
